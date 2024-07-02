6b9fcd407f35d1837d34c7b840bc80af5f3bd5a9
AnnambhotlaK <110944729+AnnambhotlaK@users.noreply.github.com>
2024-07-01 22:00:25 -0400

    Created player and movement

---
+++ actions.py
@@ -0,0 +1,14 @@
+# Handles player actions within game
+
+class Action:
+    pass
+
+class EscapeAction(Action):
+    pass
+
+class MovementAction(Action):
+    def __init__(self, dx: int, dy: int):
+        super().__init__()
+
+        self.dx = dx
+        self.dy = dy
