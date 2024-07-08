05291e8e38a6cfd8493a96ad066cba153c9b76fd
AnnambhotlaK <110944729+AnnambhotlaK@users.noreply.github.com>
2024-07-08 00:07:37 -0400

    Created enemy ai and basic combat

--- actions.py
+++ actions.py
@@ -1,91 +1,104 @@
-# Handles entity actions
 from __future__ import annotations
 
 from typing import Optional, Tuple, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from engine import Engine
-    from entity import Entity
+    from entity import Actor, Entity
+
 
 class Action:
-    def __init__(self, entity: Entity) -> None:
+    def __init__(self, entity: Actor) -> None:
         super().__init__()
         self.entity = entity
 
     @property
     def engine(self) -> Engine:
-        """
-        Return the engine this action belongs to.
-        """
+        """Return the engine this action belongs to."""
         return self.entity.gamemap.engine
-    
+
     def perform(self) -> None:
-        """
-        Perform this action with the objects needed to determine its scope.
+        """Perform this action with the objects needed to determine its scope.
 
-        'self.engine' is the scope this action is being performed in.
+        `self.engine` is the scope this action is being performed in.
 
-        'self.entity' is the object performing the action.
+        `self.entity` is the object performing the action.
 
-        Method must be overriden by Action subclass.
+        This method must be overridden by Action subclasses.
         """
         raise NotImplementedError()
+
 
 class EscapeAction(Action):
     def perform(self) -> None:
         raise SystemExit()
 
 
+class WaitAction(Action):
+    def perform(self) -> None:
+        pass
+
+
 class ActionWithDirection(Action):
-    def __init__(self, entity: Entity, dx: int, dy: int):
+    def __init__(self, entity: Actor, dx: int, dy: int):
         super().__init__(entity)
 
         self.dx = dx
         self.dy = dy
-    
+
     @property
     def dest_xy(self) -> Tuple[int, int]:
-        """
-        Returns this action's destination.
-        """
+        """Returns this actions destination."""
         return self.entity.x + self.dx, self.entity.y + self.dy
-    
+
     @property
     def blocking_entity(self) -> Optional[Entity]:
-        """
-        Return the blocking entity at this action's destination.
-        """
+        """Return the blocking entity at this actions destination.."""
         return self.engine.game_map.get_blocking_entity_at_location(*self.dest_xy)
+
+    @property
+    def target_actor(self) -> Optional[Actor]:
+        """Return the actor at this actions destination."""
+        return self.engine.game_map.get_actor_at_location(*self.dest_xy)
 
     def perform(self) -> None:
         raise NotImplementedError()
 
+
 class MeleeAction(ActionWithDirection):
     def perform(self) -> None:
-        target = self.blocking_entity
+        target = self.target_actor
         if not target:
             return  # No entity to attack.
 
-        print(f"You kick the {target.name}, much to its annoyance!")
+        damage = self.entity.fighter.power - target.fighter.defense
+
+        attack_desc = f"{self.entity.name.capitalize()} attacks {target.name}"
+        if damage > 0:
+            print(f"{attack_desc} for {damage} hit points.")
+            target.fighter.hp -= damage
+        else:
+            print(f"{attack_desc} but does no damage.")
+
 
 class MovementAction(ActionWithDirection):
     def perform(self) -> None:
         dest_x, dest_y = self.dest_xy
 
         if not self.engine.game_map.in_bounds(dest_x, dest_y):
-            return   # Destination is out of bounds.
+            return  # Destination is out of bounds.
         if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
-            return   # Destination is blocked by tile.
+            return  # Destination is blocked by a tile.
         if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-            return   # Destination is blocked by an entity.
-        
+            return  # Destination is blocked by an entity.
+
         self.entity.move(self.dx, self.dy)
 
-# BumpAction Determines whether to call MovementAction or MeleeAction.
+
 class BumpAction(ActionWithDirection):
     def perform(self) -> None:
-        if self.blocking_entity:
+        if self.target_actor:
             return MeleeAction(self.entity, self.dx, self.dy).perform()
+
         else:
-            # Moving into space without blocking entity -> MovementAction
             return MovementAction(self.entity, self.dx, self.dy).perform()
--- entity_factories.py
+++ entity_factories.py
@@ -1,25 +1,30 @@
-from entity import Entity
+from components.ai import HostileEnemy
+from components.fighter import Fighter
+from entity import Actor
 
 # The user-controlled player entity
-player = Entity(
+player = Actor(
     char = "@", 
     color = (255, 255, 255), 
     name = "Player", 
-    blocks_movement = True
+    ai_cls = HostileEnemy,
+    fighter = Fighter(hp = 30, defense = 2, power = 5)
     )
 
 # Weaker enemy, high chance of spawning
-orc = Entity(
+orc = Actor(
     char = "o", 
     color = (63, 127, 63), 
     name = "Orc", 
-    blocks_movement = True
+    ai_cls = HostileEnemy,
+    fighter = Fighter(hp = 10, defense = 0, power = 3)
     )
 
 # Stronger enemy, low chance of spawning
-troll = Entity(
+troll = Actor(
     char = "T",
     color = (0, 127, 0),
     name = "Troll",
-    blocks_movement = True
+    ai_cls = HostileEnemy,
+    fighter = Fighter(hp = 16, defense = 1, power = 4),
 )
